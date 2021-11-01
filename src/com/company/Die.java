a66a91f9834dbaa5fc341a99fd7d431ba0ef0305
Raghav Vaid <raghavvaid@billericak12.com>
2021-11-01 12:03:38 -0400

    safwf

---
+++ src/com/company/Die.java
@@ -0,0 +1,22 @@
+package com.company;
+public class Die {
+
+    private int numDots;
+
+    public int roll(){
+        return numDots = (int)(Math.random()*6) + 1;
+    }
+
+
+    public int getNumDots(){
+        return numDots;
+    }
+
+
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
+        Die die = new Die();
+        die.roll();
+        System.out.println(die.getNumDots());
+    }
+}
